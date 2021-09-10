485f88d13b3e698b600d95dcd7769de1f132c536
Rafael Furtado Rodrigues dos Santos <49576121+Syank@users.noreply.github.com>
2021-09-10 16:14:17 -0300

    Mais alguns avanços

--- src/main.py
+++ src/main.py
@@ -1,5 +1,28 @@
-a = "aa."
+from connection import ConnectionManager
 
-a = a[:-1]
+conn = ConnectionManager("localhost", "mysqluser", "pass", "database")
 
-print(a)
+
+
+
+
+def testSelect():
+    table = ""    
+
+    results = conn.executeSelect(table)
+
+    for result in results:
+        print(result)
+
+def testInsert():
+    table = ""
+    values = {"1": "a", "2": "b"}
+
+    
+    pass
+
+def testUpdate():
+    pass
+
+def testDelete():
+    pass
