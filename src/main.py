c97255aee72afbb34ea673f85ec86fb1282ffe0b
Rafael Furtado <rafs.97@hotmail.com>
2021-09-19 18:50:30 -0300

    Finalizando conexão de demonstração

--- src/main.py
+++ src/main.py
@@ -1,28 +1,71 @@
-from connection import ConnectionManager
-
-conn = ConnectionManager("localhost", "mysqluser", "pass", "database")
+import mysql.connector as mysql
 
 
+def selectExemple(cursor):
+    print("---- Exemplo de select ----")
+    sql = "select * from alunos;"
+
+    print(sql)
+    print("Resultado:")
+    cursor.execute(sql)
+
+    registros = cursor.fetchall()
+
+    for registro in registros:
+        print(registro)
+
+    print("-------------------------\n")
 
 
+def insertExemple(cursor):
+    print("---- Exemplo de insert ----")
+    sql = "insert into alunos (nome, curso) values ('Natasha', 'BDD');"
+    print(sql)
 
-def testSelect():
-    table = ""    
+    cursor.execute(sql)
 
-    results = conn.executeSelect(table)
+    print(cursor.rowcount, "Inserido com sucesso!")
+    print("-------------------------\n")
 
-    for result in results:
-        print(result)
 
-def testInsert():
-    table = ""
-    values = {"1": "a", "2": "b"}
+def updateExemple(cursor):
+    print("---- Exemplo de update ----")
+    sql = "update alunos set nome = 'Rafael Furtado' where id = 1;"
+    print(sql)
 
-    
-    pass
+    cursor.execute(sql)
 
-def testUpdate():
-    pass
+    print(cursor.rowcount, "Atualizado com sucesso!")
+    print("-------------------------\n")
 
-def testDelete():
-    pass
+
+def deleteExemple(cursor):
+    print("---- Exemplo de delete ----")
+    sql = "delete from alunos where id = 2;"
+    print(sql)
+
+    cursor.execute(sql)
+
+    print(cursor.rowcount, "Deletado com sucesso!")
+    print("-------------------------\n")
+
+
+def exemples(cursor):
+    selectExemple(cursor)
+    insertExemple(cursor)
+    updateExemple(cursor)
+    deleteExemple(cursor)
+
+    selectExemple(cursor)
+
+
+host = "192.168.1.104"
+database = "fatec"
+user = "admin"
+password = "123"
+connection = mysql.connect(host=host, database=database, user=user, password=password, connection_timeout=5)
+
+cursor = connection.cursor()
+
+exemples(cursor)
+
